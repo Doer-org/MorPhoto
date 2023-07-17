c2a30afd207507d253b16eb199b9a2dbb5e8fba8
ryoya0902 <ryuasa1192@gmail.com>
2023-07-17 11:14:34 +0900

    MLサイドのベースを作成

---
+++ morphoto/src/image_conversion.py
@@ -0,0 +1,47 @@
+import os
+import sys
+
+import torch
+from diffusers import StableDiffusionImg2ImgPipeline
+from dotenv import load_dotenv
+from omegaconf import OmegaConf
+from PIL import Image
+
+sys.path.append("configs")
+from config import DiffusionConfig
+
+
+class ImageConverter:
+    def __init__(self, diffusion_config: DiffusionConfig):
+        self.config = diffusion_config
+        self.device = torch.device(self.config.device)
+        self.model, self.generator = self._inference_setup()
+
+    def _inference_setup(self):
+        load_dotenv()
+        token = os.environ.get("HUGGING_FACE_TOKEN")
+        model = StableDiffusionImg2ImgPipeline.from_pretrained(self.config.model, use_auth_token=token)
+        model = model.to(self.device)
+        model.safety_checker = lambda images, **kwargs: (images, [False])
+        generator = torch.Generator(device=self.device).manual_seed(self.config.seed)
+        return model, generator
+
+    def convert(self, prompt: str, image, strength: float = 0.8):
+        image = image.resize(self.config.image_size)
+        image = self.model(
+            prompt=prompt,
+            image=image,
+            strength=strength,
+            generator=self.generator,
+        ).images[0]
+        return image
+
+
+if __name__ == "__main__":
+    diffusion_config = OmegaConf.create(DiffusionConfig)
+    prompt = "best quality masterpiece makoto shinkai "
+    image_path = "data/sample/nijika.png"
+    image = Image.open(image_path)
+    converter = ImageConverter(diffusion_config)
+    image = converter.convert(prompt, image)
+    image.save("result.png")
---
+++ morphoto/src/prompt_conversion.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+import openai
+from dotenv import load_dotenv
+from omegaconf import OmegaConf
+
+sys.path.append("configs")
+from config import ChatGPTConfig
+
+
+class PromptConverter:
+    def __init__(self, chatgpt_config: ChatGPTConfig):
+        self.config = chatgpt_config
+        load_dotenv()
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    def convert(self, sentence: str) -> str:
+        system_setting = {"role": "system", "content": self.config.system_prompt}
+        user_input = {"role": "user", "content": sentence}
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[system_setting, user_input])
+        response = response.choices[0].message.content
+        return response
+
+
+if __name__ == "__main__":
+    example_1 = "Make it a character-like image of a cute anime"
+    print("入力文")
+    print(example_1)
+    chatgpt_config = OmegaConf.create(ChatGPTConfig)
+    prompt_converter = PromptConverter(chatgpt_config)
+    prompt = prompt_converter.convert(example_1)
+    print("プロンプト")
+    print(prompt)
