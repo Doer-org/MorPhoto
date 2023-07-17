dcfd0c63518be5107b6ca4f891a41c62382e20ad
yasuda.chuma <86759515+yach36@users.noreply.github.com>
2023-07-17 23:58:45 +0900

    feat(client): 結果ページのシンプルな画面を作成

--- client/src/app/result/page.tsx
+++ client/src/app/result/page.tsx
@@ -1,5 +1,48 @@
 const Page = () => {
-  return <>Result Page</>;
+  return (
+    <>
+      <h2>Result Page</h2>
+      <div>
+        <span>元画像</span>
+        <div>
+          <img
+            src="https://media.discordapp.net/attachments/1130117141228703824/1130117145217478737/result_0.png?width=500&height=560"
+            alt=""
+            width={160}
+            height={160}
+          />
+        </div>
+      </div>
+      <div>
+        <span>プロンプト</span>
+        <p>花火大会</p>
+      </div>
+      <div>
+        <span>結果画像</span>
+        <div>
+          <img
+            src="https://media.discordapp.net/attachments/1126485571108360313/1130108343839621140/result.png?width=662&height=662"
+            alt=""
+            width={160}
+            height={160}
+          />
+        </div>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <a href="#">共有する</a>
+          </li>
+          <li>
+            <a href="#">さらに画像を変化する</a>
+          </li>
+          <li>
+            <a href="#">タイムラインへ</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Page;
