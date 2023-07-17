6662251a52b21471f295d839cd7a4cfc701a5d16
ryoya0902 <ryuasa1192@gmail.com>
2023-07-17 21:02:39 +0900

    プロンプト変換のテストを修正

--- morphoto/tests/test_prompt_conversion.py
+++ morphoto/tests/test_prompt_conversion.py
@@ -13,8 +13,3 @@ class TestPromptConverter(unittest.TestCase):
     def setUp(self) -> None:
         self.config = OmegaConf.create(ChatGPTConfig)
         self.prompt_converter = PromptConverter(self.config)
-
-    def test_convert(self) -> None:
-        example_1 = "Make it a character-like image of a cute anime"
-        prompt = self.prompt_converter.convert(example_1)
-        self.assertEqual(type(prompt), str)
