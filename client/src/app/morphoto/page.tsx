cb6d3fd3d27a706796b72eb214880a1dfa94448b
yasuda.chuma <86759515+yach36@users.noreply.github.com>
2023-07-18 14:59:56 +0900

    feat(client): morphotoページ作成

--- client/src/app/morphoto/page.tsx
+++ client/src/app/morphoto/page.tsx
@@ -1,5 +1,81 @@
 const Page = () => {
-  return <>Morphoto Page</>;
+  return (
+    <>
+      <h2>Morphoto Page</h2>
+      <div style={{ display: "flex" }}>
+        <div>
+          <span>入力</span>
+          <div>
+            <img
+              src="https://cdn.discordapp.com/attachments/1130117141228703824/1130117281016451072/result_0.png"
+              alt=""
+              width={240}
+              height={240}
+            />
+          </div>
+        </div>
+        <div>
+          <span>結果</span>
+          <div>
+            <img
+              src="https://cdn.discordapp.com/attachments/1126485571108360313/1129936798685069373/result_1.0.png"
+              alt=""
+              width={240}
+              height={240}
+            />
+          </div>
+        </div>
+      </div>
+      <div style={{ display: "flex" }}>
+        <div>
+          <span>入力</span>
+          <div>
+            <img
+              src="https://cdn.discordapp.com/attachments/1130117141228703824/1130117281016451072/result_0.png"
+              alt=""
+              width={240}
+              height={240}
+            />
+          </div>
+        </div>
+        <div>
+          <span>結果</span>
+          <div>
+            <img
+              src="https://cdn.discordapp.com/attachments/1126485571108360313/1129936798685069373/result_1.0.png"
+              alt=""
+              width={240}
+              height={240}
+            />
+          </div>
+        </div>
+      </div>
+      <div style={{ display: "flex" }}>
+        <div>
+          <span>入力</span>
+          <div>
+            <img
+              src="https://cdn.discordapp.com/attachments/1130117141228703824/1130117281016451072/result_0.png"
+              alt=""
+              width={240}
+              height={240}
+            />
+          </div>
+        </div>
+        <div>
+          <span>結果</span>
+          <div>
+            <img
+              src="https://cdn.discordapp.com/attachments/1126485571108360313/1129936798685069373/result_1.0.png"
+              alt=""
+              width={240}
+              height={240}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Page;
