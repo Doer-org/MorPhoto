92c47ecab220a2cf2e257cb1b84e58a8eece9fd3
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-08-05 13:35:15 +0900

    test(server): add api test

--- server/api/Api.Test/Tests.fs
+++ server/api/Api.Test/Tests.fs
@@ -4,14 +4,57 @@ open Xunit
 open FsUnit.Xunit
 open System.Net
 open FsHttp
+open System
+
+let endpoint = "http://localhost:8091"
+
 
 [<Fact>]
 let health () =
     let resp =
         http {
-            GET "http://localhost:8080/health"
+            GET $"{endpoint}/health"
             CacheControl "no-cache"
         }
         |> Request.send
 
     resp.statusCode |> should equal HttpStatusCode.OK
+
+[<Fact>]
+let users () =
+    let resp =
+        http {
+            GET $"{endpoint}/users"
+            CacheControl "no-cache"
+        }
+        |> Request.send
+
+    resp.statusCode |> should equal HttpStatusCode.OK
+
+[<Fact>]
+let morphoto () =
+    let key = "test_" + Guid.NewGuid().ToString()
+
+    let resp =
+        http {
+            POST $"{endpoint}/morphoto"
+            CacheControl "no-cache"
+            body
+
+            jsonSerialize
+                {| morphoto_id = key
+                   img_url = key
+                   parent_id = key |}
+        }
+        |> Request.send
+
+    resp.statusCode |> should equal HttpStatusCode.OK
+
+// let resp =
+//     http {
+//         GET $"{endpoint}/morphoto/test"
+//         CacheControl "no-cache"
+//     }
+//     |> Request.send
+
+// resp.statusCode |> should equal HttpStatusCode.OK
