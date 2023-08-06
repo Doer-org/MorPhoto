0f61c00665884f76efeb37a2773dfb9057f5b7fe
yasuda.chuma <86759515+yach36@users.noreply.github.com>
2023-08-06 23:00:23 +0900

    feat(client): 共通レイアウトにヘッダーを設定

---
+++ client/src/app/_component/mor-photo-header/mor-photo-header.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Header } from "@/ui";
+import { ComponentPropsWithoutRef, FC, forwardRef } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import * as styles from "./mor-photo-header.css";
+
+type Props = ComponentPropsWithoutRef<"header">;
+
+export const MorPhotoHeader = forwardRef<HTMLElement, Props>(({}, ref) => {
+  return (
+    <Header
+      className={styles.headerStyle}
+      leftChildren={
+        <h1 className={styles.logoStyle}>
+          <Link href={"/"}>
+            <span className={styles.logoBlueStyle}>Mor</span>Photo
+          </Link>
+        </h1>
+      }
+      rightChildren={
+        <Image src={"/assets/nijika1.png"} width={20} height={20} alt="ロゴ" />
+      }
+      ref={ref}
+    />
+  );
+});
+
+MorPhotoHeader.displayName = "MorPhotoHeader";
--- client/src/ui/header/header.tsx
+++ client/src/ui/header/header.tsx
@@ -1,28 +1,31 @@
-import { FC, ReactNode } from "react";
+import { ComponentPropsWithoutRef, FC, ReactNode, forwardRef } from "react";
 
-type Props = {
+type Props = ComponentPropsWithoutRef<"header"> & {
   leftChildren: ReactNode;
   rightChildren: ReactNode;
 };
 
-const _Header: FC<Props> = ({ leftChildren, rightChildren }: Props) => {
-  return (
-    <header
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        right: 0,
-        background: "#aaaaaa",
-      }}
-    >
-      {/* left */}
-      <div style={{ float: "left" }}>{leftChildren}</div>
-      {/* right */}
-      <div style={{ float: "right" }}>{rightChildren}</div>
-    </header>
-  );
-};
+const _Header = forwardRef<HTMLElement, Props>(
+  ({ leftChildren, rightChildren, ...props }, ref) => {
+    return (
+      <header
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+        }}
+        {...props}
+        ref={ref}
+      >
+        {/* left */}
+        <div style={{ float: "left" }}>{leftChildren}</div>
+        {/* right */}
+        <div style={{ float: "right" }}>{rightChildren}</div>
+      </header>
+    );
+  }
+);
 
 _Header.displayName = "Header";
 
