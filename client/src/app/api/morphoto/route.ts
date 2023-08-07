c5e2c52dd901a736f1b6731f5c45d1f14b0ead47
yasuda.chuma <86759515+yach36@users.noreply.github.com>
2023-08-07 23:00:26 +0900

    fix(client): 動作確認ページ削除

--- client/src/app/api/morphoto/route.ts
+++
@@ -1,7 +0,0 @@
-import { readAllMorphotos } from "@/api";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const data = await readAllMorphotos();
-  return NextResponse.json(data);
-}
