ce130418cbcb153acfffcdf045745b5abad19289
yasuda.chuma <86759515+yach36@users.noreply.github.com>
2023-08-07 20:20:10 +0900

    feat(client): core api

---
+++ client/src/api/core/api-client.ts
@@ -0,0 +1,61 @@
+import Ajv, { AnySchema } from "ajv";
+
+import { Result } from "./types";
+import { JTDDataType } from "ajv/dist/core";
+
+export type ResponseError = {
+  status: number;
+  message: string;
+};
+
+const ajv = new Ajv({
+  allErrors: false,
+  strict: false,
+});
+
+const resp2result = async <T extends AnySchema>(
+  resp: Response
+): Promise<Result<T, ResponseError>> => {
+  const data = (await resp.json()) as T;
+  const validate = ajv.compile<JTDDataType<T>>(data);
+  if (!resp.ok) {
+    return {
+      type: "error",
+      error: {
+        status: resp.status,
+        message: resp.statusText,
+      },
+    };
+  } else if (!validate(data)) {
+    return {
+      type: "error",
+      error: {
+        status: resp.status,
+        message: JSON.stringify(validate.errors),
+      },
+    };
+  }
+  return { type: "ok", value: data };
+};
+
+export const apiClient = {
+  get: async <T extends AnySchema>(url: string) => {
+    const data = await fetch(url, {
+      method: "GET",
+    });
+    return await resp2result<T>(data);
+  },
+  post: async <T extends AnySchema>(
+    url: string,
+    body: Record<string, unknown> | Record<string, unknown>[]
+  ) => {
+    const data = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    return await resp2result<T>(data);
+  },
+};
