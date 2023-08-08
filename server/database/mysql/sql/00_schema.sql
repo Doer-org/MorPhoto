159193e82050f39a6e6d5e4a58c724df0567c309
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-08-08 20:53:12 +0900

    chore(server): update

--- server/database/mysql/sql/00_schema.sql
+++ server/database/mysql/sql/00_schema.sql
@@ -5,9 +5,6 @@ CREATE TABLE `User` (
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-INSERT INTO User (user_id, user_name, image_url) VALUES ('abc', 'user1', 'https://www.google.com');
-INSERT INTO User (user_id, user_name, image_url) VALUES ('cdf', 'user2', 'https://www.google.com');
-
 CREATE TABLE `Morphoto`(
   `morphoto_id`   varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `img_url`       varchar(255) COLLATE utf8mb4_bin NOT NULL,
@@ -15,19 +12,9 @@ CREATE TABLE `Morphoto`(
   PRIMARY KEY (`morphoto_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-INSERT INTO Morphoto (morphoto_id, img_url, parent_id) VALUES ('abc', 'https://www.google.com', NULL);
-INSERT INTO Morphoto (morphoto_id, img_url, parent_id) VALUES ('cdf', 'https://www.google.com', NULL);
-INSERT INTO Morphoto (morphoto_id, img_url, parent_id) VALUES ('ghi', 'https://www.google.com', 'abc');
-INSERT INTO Morphoto (morphoto_id, img_url, parent_id) VALUES ('jkl', 'https://www.google.com', 'cdf');
-
 CREATE TABLE `MorphotoLog`( 
   `morphoto_id`   varchar(255) COLLATE utf8mb4_bin NOT NULL, 
   `created_at`    datetime NOT NULL,
   `view_count`    int NOT NULL,
   PRIMARY KEY (`morphoto_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-INSERT INTO MorphotoLog (morphoto_id, created_at, view_count) VALUES ('abc', '2020-01-01 00:00:00', 1);
-INSERT INTO MorphotoLog (morphoto_id, created_at, view_count) VALUES ('cdf', '2020-01-01 00:00:01', 2);
-INSERT INTO MorphotoLog (morphoto_id, created_at, view_count) VALUES ('ghi', '2020-01-01 00:00:02', 3);
-INSERT INTO MorphotoLog (morphoto_id, created_at, view_count) VALUES ('jkl', '2020-01-01 00:00:03', 4);
