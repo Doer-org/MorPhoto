3238b8fabda5b4bf6f36a6a591e47ad529d260cb
yasuda.chuma <86759515+yach36@users.noreply.github.com>
2023-08-08 13:12:30 +0900

    fix(client): パス修正

--- client/src/api/inference/index.ts
+++ client/src/api/inference/index.ts
@@ -3,7 +3,4 @@ import { apiClient } from "../core";
 import { InferenceCreate, InferenceResponse } from "./types";
 
 export const createInference = async (inference: InferenceCreate) =>
-  await apiClient.post<InferenceResponse>(
-    `${env.ML_URL}/inference}`,
-    inference
-  );
+  await apiClient.post<InferenceResponse>(`${env.ML_URL}/inference`, inference);
