256717d911343f28da31882d95ff8bff9d3d39aa
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-08-09 09:12:06 +0900

    feat(ml): 推論せずにオウム返しするデバッグ用オプションを追加

--- morphoto/src/models.py
+++ morphoto/src/models.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class InferenceRequest(BaseModel):
     image: str
     prompt: str
     strength: float
+    is_mock: Optional[bool]
