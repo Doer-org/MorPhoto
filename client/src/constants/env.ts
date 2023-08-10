71c230a95f24ad260e91ef46ac57e606387618cf
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-08-10 23:35:45 +0900

    chore(client): update-env

--- client/src/constants/env.ts
+++ client/src/constants/env.ts
@@ -1,9 +1,4 @@
 export const env = {
   CLIENT_URL: process.env.NEXT_PUBLIC_CLIENT_URL || "",
   SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  ML_URL: process.env.NEXT_PUBLIC_ML_URL || "",
-  GCP_PROJECT_ID: process.env.GCP_PROJECT_ID || "",
-  GCP_BUCKET_NAME: process.env.GCP_BUCKET_NAME || "",
-  GCP_CREDENTIALS: process.env.GCP_CREDENTIALS || "",
-  GCS_URL: "https://storage.googleapis.com/morphoto_strage",
 };
