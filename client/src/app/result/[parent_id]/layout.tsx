60ef0ad354168a2d24226da792ea5f61bc6859a5
yasuda.chuma <86759515+yach36@users.noreply.github.com>
2023-08-13 23:07:06 +0900

    feat(client): 親OGP引継ぎ

--- client/src/app/result/[parent_id]/layout.tsx
+++ client/src/app/result/[parent_id]/layout.tsx
@@ -1,4 +1,3 @@
-import { readMorphoto } from "@/api";
 import { Metadata, ResolvingMetadata } from "next";
 import { ReactNode } from "react";
 
@@ -6,19 +5,15 @@ export default function ResultLayout({ children }: { children: ReactNode }) {
   return <>{children}</>;
 }
 
-type Props = {
-  params: { parent_id: string };
-};
+export async function generateMetadata(
+  {},
+  parent?: ResolvingMetadata
+): Promise<Metadata> {
+  const previousImages = (await parent)?.openGraph?.images || [];
 
-// export async function generateMetadata({ params: {parent_id} }: Props, parent?: ResolvingMetadata): Promise<Metadata> {
-//   const morphoto = await readMorphoto(parent_id)
-//   const previousImages = (await parent).openGraph?.images || []
-
-//   return {
-//     title: post.title,
-//     description: post.summary,
-//     openGraph: {
-//       images: ['/some-specific-page-image.jpg', ...previousImages],
-//     },
-//   }
-// }
+  return {
+    openGraph: {
+      images: [...previousImages],
+    },
+  };
+}
