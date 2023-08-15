e16d5905dea239e2d8656bc990dd7c6b9440ecf6
ryoya0902 <ryuasa1192@gmail.com>
2023-08-15 22:15:20 +0900

    fix(ml): プロンプト変換のバグを修正

--- morphoto/src/prompt_converter.py
+++ morphoto/src/prompt_converter.py
@@ -11,19 +11,22 @@ class PromptConverter:
         self.config = chatgpt_config
         load_dotenv()
         openai.api_key = os.getenv("OPENAI_API_KEY")
+        self.ignore_keywords = self.config.ignore_keywords
 
     def convert(self, sentence: str) -> str:
         system_setting = {"role": "system", "content": self.config.system_prompt}
         user_input = {"role": "user", "content": sentence}
         response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[system_setting, user_input])
         response = response.choices[0].message.content
+        if any(word in response for word in self.ignore_keywords):
+            return sentence
         return response
 
 
 if __name__ == "__main__":
     from omegaconf import OmegaConf
 
-    example_1 = "Make it a character-like image of a cute anime"
+    example_1 = "狐狐狐狐"
     print("入力文")
     print(example_1)
     chatgpt_config = OmegaConf.create(ChatGPTConfig)
